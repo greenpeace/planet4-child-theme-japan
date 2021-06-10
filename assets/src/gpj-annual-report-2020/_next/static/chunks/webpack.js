66e92049d31802e78cb2da3f9bcf22835a68b2a4
tkhg <28704185+tkhg@users.noreply.github.com>
2021-06-11 03:18:04 +0800

    fix: hero img not shown

--- assets/src/gpj-annual-report-2020/_next/static/chunks/webpack.js
+++ assets/src/gpj-annual-report-2020/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff62a1f06f4be5275d10";
+/******/ 	var hotCurrentHash = "b5376bf489aa724fe6b6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
