9eb65410d054eddbb21d3269a62b90397d2372b4
tkhg <28704185+tkhg@users.noreply.github.com>
2021-10-08 06:56:50 +0800

    feat: add custom page template

--- functions.php
+++ functions.php
@@ -31,3 +31,65 @@ if($is_debug){
  */
 require_once __DIR__ . '/classes/class-p4jp-loader.php';
 P4JP_Theme_Loader::get_instance();
+
+
+
+add_action('wp_enqueue_scripts', 'deregister_parent_styles', 11);
+
+function deregister_parent_styles(){
+	if(is_page_template('page-templates/custom.php')) {
+		wp_dequeue_style('parent_style');
+		wp_deregister_style('parent_style');
+		// wp_dequeue_style('bootstrap');
+		// wp_deregister_style('bootstrap');
+	}
+}
+
+add_action( 'cmb2_admin_init', 'cmb2_custom_page_settings_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_custom_page_settings_metaboxes() {
+
+    /**
+     * Initiate the metabox
+     */
+    $cmb = new_cmb2_box( array(
+        'id'            => 'p4jp_custom_page_settings',
+        'title'         => __( 'Custom Page Settings', 'cmb2' ),
+        'object_types'  => array( 'page' ), // Post type
+				'show_on'				=> array( 'key' => 'page-template', 'value' => 'page-templates/custom.php' ),
+        'context'       => 'normal',
+        'priority'      => 'high',
+        'show_names'    => true, // Show field names on the left
+        // 'cmb_styles' => false, // false to disable the CMB stylesheet
+				'closed'     => true, // Keep the metabox closed by default
+    ) );
+
+    // Regular text field
+    $cmb->add_field( array(
+        'name'       => __( 'Custom JS', 'cmb2' ),
+        'desc'       => __( 'URL / relative path to additional JS file', 'cmb2' ),
+        'id'         => 'p4jp_custom_page_js',
+        'type'       => 'text',
+				// 'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+        // 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+        // 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+        // 'on_front'        => false, // Optionally designate a field to wp-admin only
+        // 'repeatable'      => true,
+    ) );
+
+    $cmb->add_field( array(
+        'name'       => __( 'Custom CSS', 'cmb2' ),
+        'desc'       => __( 'URL / relative path to additional CSS file', 'cmb2' ),
+        'id'         => 'p4jp_custom_page_css',
+        'type'       => 'text',
+				// 'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+        // 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+        // 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+        // 'on_front'        => false, // Optionally designate a field to wp-admin only
+        // 'repeatable'      => true,
+    ) );
+    // Add other metaboxes as needed
+
+}
