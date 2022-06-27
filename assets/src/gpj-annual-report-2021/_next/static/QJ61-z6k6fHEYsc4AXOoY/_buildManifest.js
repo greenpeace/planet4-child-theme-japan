e65d330726126113e75cfe2e94af978ca07ca1d2
tkhg <28704185+tkhg@users.noreply.github.com>
2022-06-28 00:59:30 +0800

    feat: update annual report 2021 pdf

--- assets/src/gpj-annual-report-2021/_next/static/QJ61-z6k6fHEYsc4AXOoY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/65-2d9204b87bc47b5c.js","static/chunks/pages/index-a4090f1a370065bf.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
