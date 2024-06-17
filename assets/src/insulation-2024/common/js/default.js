cd449d81a0396f8b254f4aeadafe0afec36adf1d
chiyoda hiroki <chiyoda@bridgedesign.jp>
2024-06-17 21:44:41 +0900

    change_font

--- assets/src/insulation-2024/common/js/default.js
+++ assets/src/insulation-2024/common/js/default.js
@@ -7,7 +7,16 @@ $(function () {
     // var sp_mode = 560;//
     //今回はヘッダ考慮しない
     // var headerhight = $('header').height() + 50; 
-    var headerhight = 0; 
+    var headerhight = 0;
+
+    (function(d) {
+        var config = {
+        kitId: 'dfu5msm',
+        scriptTimeout: 3000,
+        async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f%7C%7Ca&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
 
     /****************/
     /*アンカーリンクがあればそこまでスクロール*/
